4328090a8e3370a5c0130dc86ccd26c1269be39b
Dunadan <sosiska7150@gmail.com>
2023-03-16 02:02:40 +0300

    game

---
+++ Utils/Randomer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sosis on 15.03.2023.
+//
+
+#include "Randomer.h"
---
+++ Utils/Randomer.h
@@ -0,0 +1,15 @@
+//
+// Created by sosis on 15.03.2023.
+//
+
+#ifndef TASK_2_RANDOMER_H
+#define TASK_2_RANDOMER_H
+
+
+class Randomer
+{
+
+};
+
+
+#endif //TASK_2_RANDOMER_H
